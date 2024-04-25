c5741a582a0391d3ec00770a7d7a21d53ed9e1a0
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-04-25 12:19:06 -0300

    psicopat-ia

---
+++ view/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./../../**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "branco": "#EFEBE8",
+        "marromClaro": "#A79E8F",
+        "marrom": "#231E1A",
+        "cinzaMarrom": "#827F7B",
+        "marronzinho": "#6F6A66",
+        "cinza": "#777672"
+      },
+      fontFamily: {
+        fontTTPD: ["Libre Caslon Display", "serif"],
+        fontTTPDT: ["Libre Caslon Text", "serif"],
+        fontP: ["Poppins", "system-ui"]
+      }
+    },
+  },
+  plugins: [],
+}
+
